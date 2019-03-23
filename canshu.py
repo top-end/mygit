1989163a3050d262d369fcc5b9be5611c4a7a005
HYP <1145745355@qq.com>
2019-03-23 16:52:15 +0800

    second commit

---
+++ canshu.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+a = 400
+def test1():
+    a = 200
+    a +=1
+    print("a的值为%s:" %a)
+def test2():
+    a= 300
+    print("test2的a的值为%s:" %a)
+
+test1()
+test2()
+
